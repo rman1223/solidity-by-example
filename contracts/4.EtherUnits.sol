3d9f8f557428bc9661c519d8fcfc90af4b596b8f
karma.py10 <rman.se000@gmail.com>
2022-06-08 23:29:39 -0800

    4-Ehter and Wei

---
+++ contracts/4.EtherUnits.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract EtherUnits {
+    uint public oneWei = 1 wei;
+
+    bool public isOneWei = 1 wei == 1;
+
+    uint public oneEther = 1 ether;
+
+    bool public isOneEther = 1 ether == 1e18;
+}
