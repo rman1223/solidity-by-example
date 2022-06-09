1468386ee747a688a79d4a9de99b94e6b2b856c2
karma.py10 <rman.se000@gmail.com>
2022-06-08 23:52:46 -0800

    7-Mapping

---
+++ contracts/7.Mapping.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Mapping {
+    mapping(address => uint) public myMap;
+
+    function get(address _addr) public view returns (uint) {
+        return myMap[_addr];
+    }
+
+    function set(address _addr, uint _i) public {
+        myMap[_addr] = _i;
+    }
+
+    function remove(address _addr) public {
+        delete myMap[_addr];
+    }
+}
+
+contract NestedMapping {
+    mapping(address => mapping(uint => bool)) public nested;
+    
+    function get(address _addr1, uint _i) public view returns (bool) {
+        return nested[_addr1][_i];
+    }
+
+    function set(address _addr1, uint _i, bool _boo) public {
+        nested[_addr1][_i] = _boo;
+    }
+}
